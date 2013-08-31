ac54880f8ae8a18ee2adab02c272495eab3ba39e
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2013-08-31 18:07:11 +0200

    Clening up some comments form home#check method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,12 +6,9 @@ class HomeController < ApplicationController
 
 	def check
 		@number = params[:number]
-		# soap_response = soap_client.call(:sprawdz_przesylke, :message=>{:numer=>@number.to_s.upcase})
-		# @response = soap_response.body.to_h.to_json
 
 		carrier = PocztaPolska.new({ssl_verify_mode: :none}) # PP for some reasone do not use SSL
 		render json: Tracker.find_using(carrier).carriage(@number).body.to_h.to_json 
 
-		# render json: @response
 	end
 end
