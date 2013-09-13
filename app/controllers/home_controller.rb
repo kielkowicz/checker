fa32ceb6d69ebb96d795500164f1b5d9efde5cc6
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2013-09-13 13:02:18 +0200

    Some work on frontend

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
 	def check
 		@number = params[:number]
 		carrier = PocztaPolska.new({ssl_verify_mode: :none}) # PP for some reasone do not use SSL
-		package = Tracker.find_using(carrier).carriage(@number)
-		render json: {from: package.from, to: package.to, type: package.rodzaj_przesylki, date: package.data_nadania, status: package.in_system? }
+		@package = Tracker.find_using(carrier).carriage(@number)
+	#	render json: {from: package.from, to: package.to, type: package.rodzaj_przesylki, date: package.data_nadania, status: package.in_system? }
 	end
 end
